8d0b0a29c26845061069314c02cd02cc95d3a565
Michael Swiger <michael@swiger.dev>
2022-06-28 16:32:01 -0700

    Move gi version validation to __init__

--- cinnabar/__main__.py
+++ cinnabar/__main__.py
@@ -1,9 +1,6 @@
 from cinnabar.application import Application
-import gi
 import sys
 
-gi.require_version("Gtk", "3.0")
-gi.require_version("GtkLayerShell", "0.1")
 
 app = Application()
 app.run(sys.argv)
