a4366e0f3b842bfd0cbe2b67c8db12442cd1ebd2
Michael Swiger <michael@swiger.dev>
2022-07-07 17:30:48 -0700

    Make glib main helper behave more correctly

--- cinnabar/util.py
+++ cinnabar/util.py
@@ -4,6 +4,14 @@ from gi.repository import GLib
 
 
 def glib_call_in_main(fn: Callable):
+    # Wrap callable in a method that returns False to make it be removed from
+    # event sources after being called by main thread.
+    def inner(*args, **kwargs):
+        fn(*args, **kwargs)
+        return False
+
+    # The actual wrapper to be called returned by the decorator.
     def wrapper(*args, **kwargs):
-        GLib.idle_add(fn, *args, **kwargs)
+        GLib.idle_add(inner, *args, **kwargs)
+
     return wrapper
